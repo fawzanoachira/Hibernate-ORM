157a061595a20eb1e27ed1ae43ebf2fc13682c9d
fawzanoachira <fawzanoachira@gmail.com>
2025-08-27 18:14:52 +0530

    criteria api sample

--- criteriaapi/src/main/java/com/Insert.java
+++ criteriaapi/src/main/java/com/Insert.java
@@ -1,24 +1,32 @@
 package com;
 
+import java.util.stream.Stream;
+
 import org.hibernate.Session;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
 import org.hibernate.query.criteria.HibernateCriteriaBuilder;
+import org.hibernate.query.criteria.JpaCriteriaQuery;
+import org.hibernate.query.criteria.JpaRoot;
 
 import entity.Student;
 
 public class Insert {
     public static void main(String[] args) {
-        Student student1 = new Student();
-        student1.setStudentName("ABC");
-
-        Student student2 = new Student();
-        student2.setStudentName("DEF");
 
         Configuration cfg = new Configuration();
         cfg.configure().addAnnotatedClass(Student.class);
         Session session = cfg.buildSessionFactory().openSession();
         HibernateCriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
 
-        criteriaBuilder.createQuery(Student.class);
+        JpaCriteriaQuery<Student> query = criteriaBuilder.createQuery(Student.class);
+        JpaRoot<Student> from = query.from(Student.class);
+        query.select(from);
+
+        Query<Student> query2 = session.createQuery(query);
+        Stream<Student> resultStream = query2.getResultStream();
+        resultStream.forEach((x) -> System.out.println(x));
+
+        session.close();
     }
 }
